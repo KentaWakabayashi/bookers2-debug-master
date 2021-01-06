c836951e04dfbacf17cfb05073f075289265cfd5
EC2 Default User <ec2-user@ip-172-31-91-230.ec2.internal>
2021-01-06 11:45:37 +0000

    課題3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :books
+  has_many :book_comment, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   attachment :profile_image, destroy: false
+  
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}, uniqueness: true
