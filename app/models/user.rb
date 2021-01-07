61b2824692dd398e6f612868afd62cdf0d0f64a5
EC2 Default User <ec2-user@ip-172-31-91-230.ec2.internal>
2021-01-07 08:49:33 +0000

    課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,27 @@ class User < ApplicationRecord
   has_many :book_comment, dependent: :destroy
   has_many :favorites, dependent: :destroy
   attachment :profile_image, destroy: false
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  has_many :follower_user, through: :followed, source: :follower
+  has_many :following_user, through: :follower, source: :followed
+ 
   
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
-  validates :introduction, length: {maximum: 50}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
+  
+  def following?(other_user)
+    self.following_user.include?(other_user)
+  end
+  
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'homes#top'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+  resource :relationships, only:[:create, :destroy] 
+    get 'follower' => 'relationships#follower', as: 'follower'
+    get 'followed' => 'relationships#followed', as: 'followed'
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
